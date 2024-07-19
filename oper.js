6a5cc49f35e015892a761fd81b4f14ddcc6aacfc
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-19 11:35:25 +0200

    file commited

--- oper.js
+++ oper.js
@@ -5,3 +5,10 @@ const myName = "Elie"
 // numbers 
 const sum1 = 20+50;
 console.log(sum1);
+const cities2 = ["Manchester", "Liverpool",17,30,60];
+for (let i = 0; i < cities2.length; i++) {
+    
+    if (typeof cities2[i] ==="number")
+        console.log(cities2[i]);
+}
+    
